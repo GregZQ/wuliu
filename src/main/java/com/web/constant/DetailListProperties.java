58fa633a26d7a68bd48678086fa3f6edd2796e82
GregZQ <463803856@qq.com>
2019-01-29 10:54:57 +0800

    修改结构。

---
+++ src/main/java/com/web/constant/DetailListProperties.java
@@ -0,0 +1,76 @@
+package com.web.constant;
+
+/**
+ * Created by Jone on 2018-04-10.
+ */
+public class DetailListProperties {
+
+    public static final String PATH="fuben.xlsx";
+
+
+    public static final String endPath="end.xlsx";
+
+    public static final Integer COMPANY = 0;
+
+    public static final Integer COMPANY_LINE = 0;
+
+    public static final Integer COLUMN_LINE=16;
+
+    public static final Integer NUM=0;
+
+    public static final Integer GOOD_ID=1;
+
+    public static final Integer GOOD_NAME=2;
+
+    public static final Integer GOOD_COUNT=3;
+
+    public static final Integer GOOD_WEIGHT=4;
+
+    public static final Integer GOOD_CUBE=5;
+
+    public static final Integer PAY_COUNT=6;
+
+    public static final Integer PAY_TYPE=7;
+
+    public static final Integer COLLECTION=8;
+
+    public static final Integer SEND_TYPE=9;
+
+    public static final Integer WRITE_BACK=10;
+
+    public static final Integer TO_NAME=11;
+
+    public static final Integer TO_PLACE=12;
+
+    public static final Integer TO_PHONE=13;
+
+    public static final Integer SENDTIME=14;
+
+    public static final Integer BIG_PLACE=0;
+
+    public static final Integer BIG_PLACE_LINE=2;
+
+    public static final  Integer CARID=3;
+
+    public static final Integer CARID_LINE=3;
+
+    public static final Integer LOAD_TIME=13;
+
+    public static final Integer LOAD_TIME_LINE=3;
+
+    public static final Integer PAGE=15;
+    public static final Integer PAGELINE=3;
+
+
+    public static final Integer PAGE_COUNT=2;
+
+    public static final Integer MONEYS_ME=5;
+
+    public static final Integer MONEY_HE=9;
+
+    public static final Integer XIEHUO=2;
+
+    public static final Integer LINKPHONE=6;
+
+    public static final Integer PER_NUMS=35;
+}
---
+++ src/main/java/com/web/constant/TicketProperties.java
@@ -0,0 +1,81 @@
+package com.web.constant;
+
+/**
+ * Created by Jone on 2018-04-08.
+ */
+public class TicketProperties {
+
+    public static final String  PATH="ticket.xlsx";
+
+    public static final String pu[]={"零","壹","贰","叁","肆","伍","陆","柒","捌","玖"};
+    public static final String jin[]={"","拾","佰","仟","万"};
+
+    public static final Integer COMPANY=0;
+    public static final Integer COMPANY_LINE=0;
+
+    public static final Integer TIME=1;
+    public static final Integer TIME_LINE=1;
+
+    public static final Integer GOODS_ID=10;
+    public static final Integer GOODS_ID_LINE=1;
+
+    public static final Integer FROM_NAME=1;
+    public static final Integer FROM_NAME_LINE=2;
+
+    public static final Integer FROM_PLACE=4;
+    public static final Integer FROM_PLACE_LINE=2;
+
+    public static final Integer FROM_PHONE=10;
+    public static final Integer FROM_PHONE_LINE=2;
+
+    public static final Integer TO_NAME=1;
+    public static final Integer TO_NAME_LINE=3;
+
+    public static final Integer TO_PLACE=4;
+    public static final Integer TO_LACE_LINE=3;
+
+    public static final Integer TO_PHONE=10;
+    public static final Integer TO_PHONE_LINE=3;
+
+
+    public static final Integer GOODS_NAME=1;
+    public static final Integer GOODS_NAME_LINE=4;
+
+    public static final Integer GOODS_COUNT=3;
+    public static final Integer GOODS_COUNT_LINE=4;
+
+    public static final Integer GOODS_CUBE=5;
+    public static final Integer GOODS_CUBE_LINE=4;
+
+    public static final Integer GOODS_WEIGHT=9;
+    public static final Integer GOODS_WEIGHT_LINE=4;
+
+    public static final Integer GOODS_UNITPRICE=11;
+    public static final Integer GOODS_UNITPRICE_LINE=4;
+
+
+    public static final Integer SEND_TYPE=1;
+    public static final Integer SEND_TYPE_LINE=5;
+
+    public static final Integer WRITE_BACK=3;
+    public static final Integer WRITE_BACK_LINE=5;
+
+    public static final Integer PAY_TYPE=5;
+    public static final Integer PAY_TYPE_LINE=5;
+
+
+    public static final Integer PAY_COUNT=10;
+    public static final Integer PAY_COUNT_LINE=5;
+
+    public static final Integer COLLECTION_BIG=3;
+    public static final  Integer COLLECTION_SMALL=10;
+    public static final Integer COLLECTION_LINE=6;
+
+
+    public static final Integer CONSIGNEE=1;
+    public static final Integer CONSIGNEE_LINE=7;
+
+
+    public static final Integer CONSIGNEE_PHONE=5;
+    public static final Integer CONSIGNEE_PHONE_LINE=7;
+}
